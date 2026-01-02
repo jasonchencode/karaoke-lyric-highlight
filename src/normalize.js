e96c952e9388ad877c45bb0dfc4851d6f4fcae46
jasonchencode <jasonchenn67@gmail.com>
2026-01-02 16:44:08 -0400

    add normalize.js

--- src/normalize.js
+++ src/normalize.js
@@ -1,1 +1,8 @@
-
+export function normalizeLyrics(text) {
+    return text
+        .toLowerCase()
+        .replace(/[^a-z0-9\s']/g, "") // only keeps alphanumeric, whitespace, and single quote characters
+        .split(/\s+/) // splits at whitespace characters
+        .filter(Boolean);
+}
+ 

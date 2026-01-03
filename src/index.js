55493b37d6cac87bac0c32b9eea04d7af14d0b19
jasonchencode <jasonchenn67@gmail.com>
2026-01-02 21:18:53 -0400

    incorporate align.js to index.js

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 import { execSync } from "child_process";
 import fs from "fs";
+
 import { normalizeLyrics } from "./normalize.js";
+import { alignLyrics } from "./align.js";
 
 console.log("Running Whisper transcription...");
 
@@ -10,9 +12,23 @@ execSync(
     { stdio: "inherit" }
 );
 
+// Whisper output json file
+const whisperWords = JSON.parse(fs.readFileSync("output/whisper_words.json", "utf8"));
+
+console.log("Here are the first 5 transcribed words:");
+console.log(whisperWords.slice(0, 5));
+
+// Normalize actual lyrics
 const lyricsText = fs.readFileSync("data/lyrics.txt", "utf8");
 const lyricWords = normalizeLyrics(lyricsText);
 
 console.log(`Loaded ${lyricWords.length} actual lyric words`);
-console.log("Here are the first 10 words:");
-console.log(lyricWords.slice(0, 10));
+console.log("Here are the first 5 lyrics:");
+console.log(lyricWords.slice(0, 5));
+
+// Align lyrics to audio
+const aligned = alignLyrics(lyricWords, whisperWords);
+
+fs.writeFileSync("output/aligned_words.json", JSON.stringify(aligned, null, 2));
+
+console.log(`Aligned ${aligned.length} lyric words`);

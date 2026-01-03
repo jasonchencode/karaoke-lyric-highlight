0e9e1498ec7ce4e30bc4118a219928699dac9742
jasonchencode <jasonchenn67@gmail.com>
2026-01-02 21:16:21 -0400

    add initial align.js

--- src/align.js
+++ src/align.js
@@ -1,1 +1,33 @@
+import stringSimilarity from "string-similarity"
+import { normalizeTranscription } from "./normalize.js";
 
+export function alignLyrics(lyrics, whisperWords, threshold = 0.85) {
+    const aligned = [];
+    let l = 0; // lyric pointer (actual lyrics)
+    let w = 0; // whisper pointer
+
+    while (l < lyrics.length && w < whisperWords.length) {
+        const lyricWord = lyrics[l];
+        // Since whisperWords is a list of objects, extract just the word text
+        const spokenWord = normalizeTranscription(whisperWords[w].word);
+
+        const  similarity = stringSimilarity.compareTwoStrings(
+            lyricWord,
+            spokenWord
+        );
+
+        if (lyricWord === spokenWord || similarity >= threshold) {
+            aligned.push({
+                word: lyricWord,
+                start: whisperWords[w].start,
+                end: whisperWords[w].end
+            });
+            w++;
+            l++;
+        } else {
+            w++;
+        }
+    }
+
+    return aligned;
+}
